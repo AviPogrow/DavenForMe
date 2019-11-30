2cd07334b6aa210f0c1ed439d8ce484d32e8da17
Avi Pogrow <rpogrow@gmail.com>
2019-11-30 16:07:31 -0500

    NameTableViewCell - Remove commented code and Unused function

--- Daven4Me/Daven4Me/Views/NameTableViewCell.swift
+++ Daven4Me/Daven4Me/Views/NameTableViewCell.swift
@@ -12,9 +12,7 @@ class NameTableViewCell: UITableViewCell {
 
     @IBOutlet weak var nameLabel: UILabel!
     
-    
     @IBOutlet weak var timeIntervalLabel: UILabel!
-    
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -26,35 +24,15 @@ class NameTableViewCell: UITableViewCell {
         nameLabel.adjustsFontSizeToFitWidth = true
     }
     
-    
     override func layoutSubviews() {
         super.layoutSubviews()
         layer.borderWidth = 0.75
         layer.borderColor = UIColor.red.cgColor
         layer.cornerRadius = 25.0
         layer.masksToBounds = true 
-        //layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+       
         layer.shadowRadius = 5.0
         layer.shadowOpacity = 0.33
         
-        //layer.masksToBounds = false
-                // layer.shadowOffset = CGSize(width: 0.5, height: 0.5)
-                 //layer.shadowRadius = 0.5
-                   //    layer.shadowOpacity = 0.85
-        
-        //nameLabel.layer.borderWidth = 0.3
-        //nameLabel.layer.borderColor = UIColor.red.cgColor
-        //nameLabel.layer.cornerRadius = 20.0
-        //nameLabel.layer.masksToBounds = false
-        //nameLabel.layer.shadowOffset = CGSize(width: -0.5, height: 0.5)
-         //nameLabel.layer.shadowRadius = 5
-        //nameLabel.layer.shadowOpacity = 0.3
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
