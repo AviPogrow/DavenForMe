bc165b6a2ad87973b922d6b45910df9077b7bf21
Avi Pogrow <rpogrow@gmail.com>
2019-12-02 15:10:13 -0500

    RootViewController- Embedded SplitViewController inside a navController and embedded Nav Controller in a Tab Bar Controller

--- Daven4Me/Daven4Me/AppDelegate.swift
+++ Daven4Me/Daven4Me/AppDelegate.swift
@@ -21,7 +21,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate  {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
             guard
             
-            let splitViewController = window!.rootViewController as? UISplitViewController,
+        let rootTabController = window?.rootViewController as? UITabBarController,
+        let rootNavController = rootTabController.viewControllers?.first as? UINavigationController,
+            
+        let splitViewController = rootNavController.topViewController as? UISplitViewController,
             
             let masterNavController = splitViewController.viewControllers.first as? UINavigationController,
             
